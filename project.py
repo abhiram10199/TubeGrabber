1a58a50fe0df5e101ee60c180d9d565f93a6c53f
Abhiram Bhat <abhir@Abhiram-Lenovo.(none)>
2024-03-27 13:23:52 +0000

    Added a sorting mechanism, added in a few other file types

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+# @author : Abhiram
+
 from pytube import YouTube
 
 # Prints pretty lines for cleaner terminal look
@@ -16,13 +18,13 @@ class ReadURLs():
         return urls
 
 
-class VideoType():
+class VideoType:
     def __init__(self):
         pass
 
     # Promt the user to choose one of the following options
     def chooseType(self):
-        print("Choose one of the following numbers to specifiy the type of file to download\n1. (NOT IMPLEMENTED) Audio Only\n2. Adaptive (Audio & Video split) [Highest Resolution]\n3. (NOT IMPLEMENTED) Progressive (Audio & Video in 1 file) [NOT HIGHEST QUALITY]")
+        print("Choose one of the following numbers to specifiy the type of file to download\n1. (NOT IMPLEMENTED) Audio Only Best\n2. Audio Only 50kbps\n3. Highest Resolution\n4. 1080p\n5. 720p\n")
         self.type = input("Number: ")
         decorationPrints()
 
@@ -31,24 +33,41 @@ class VideoType():
 
 
 # Get only the sources that match with chosen type
-class VideoFilter(type):
+class VideoFilter:
     def __init__(self, type, link):
         self.type = type
+        self.link = link
 
     def filter(self):
-        if type == 1:
-            return YouTube().streams.filter(only_audio=True)
-        elif type == 2:
-            return YouTube().streams.filter(only_video=True)
-        elif type == 3:
-            return YouTube().streams.filter(progressive=True)
+        if type == 1:              # Audio only best
+            return YouTube(self.link).streams.filter(type = "audio").first()
+        elif type == 2:            # 50kbps audio only
+            itag = 249
+            return YouTube(self.link).streams.get_by_itag(itag)
+        elif type == 3:            # Highest resolution
+            return YouTube(self.link).streams.filter(progressive = True, file_extension = "mp4").first()
+        elif type == 4:            # 1080p video
+            itag = 137
+            return YouTube(self.link).streams.get_by_itag(itag)
+        elif type == 5:            # 720p video
+            itag = 136
+            return YouTube(self.link).streams.get_by_itag(itag)
         else:
             print("Invalid choice!!")
             return None
 
 
 class Downloader():
-    ...
+    def __init__(self):
+        pass
+
+    def download(self, video, downloadPath):
+        try:
+            video.download(downloadPath)
+        except:
+            print("Error!!")
+
+        
 
 # Gets the path for the download folder
 def getFolderPath():
@@ -58,33 +77,23 @@ def getFolderPath():
     return downloadFolderPath
 
 
-# Downloads the video!
-def downloadVideo(link: str, downloadPath: str): 
-    # video =  YouTube(link).streams.get_highest_resolution()
-   
-    try:
-        video.download(downloadPath)
-    except:
-        print("Error!!")
-
-
 # MAIN METHOD
 def main():
 
+    # Ask user to choose type of video to download & Get the type of video to download
     decorationPrints()
-    # videoType()
+    VideoType().chooseType() 
+    type = VideoType().getType()
+
+    # Ask user to select download folder & Read URLs from file, stored in a list 
     decorationPrints()
-    
-    # Ask user to select download folder
     downloadFolderPath = getFolderPath()
-    
-    # Read URLs from file
     urls = ReadURLs().readURLs()
 
-    # Iterate through list of URLs and download each
+    # Iterate through list of URLs and download each video
+    decorationPrints()
     for url in urls:
-        downloadVideo(url, downloadFolderPath)
-
+        VideoFilter(type, url).filter().download(downloadFolderPath)
     print("ALL VIDEOS DOWNLOADED!!")
     decorationPrints()
 
